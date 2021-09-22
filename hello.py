92b1bdea4b77fc9d7134db9f68d418b9ed211b75
Krishna Solanki <krishna@krishna.local>
2021-09-22 18:07:16 -0400

    Added form with validation and sessions - as per p1 for Activity 4

--- hello.py
+++ hello.py
@@ -1,16 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+
 from flask_moment import Moment
 from datetime import date, datetime
 
+from flask_wtf import Form
+from wtforms import StringField, SubmitField 
+from wtforms.validators import Required
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'xx8x9jn1odncasjni3bdin'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()]) 
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
     return render_template('index.html',
-                           current_time=datetime.utcnow())
+                            form = form, 
+                            name = session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
